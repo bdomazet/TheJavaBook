af8d396abf5a0184fe282f9c9547d9a6b6b12df1
bdomazet <bdomazet88@gmail.com>
2020-12-12 22:56:30 +0100

    Initial Git commit of The Java Book

---
+++ src/CharDemo2.java
@@ -0,0 +1,19 @@
+public class CharDemo2 {
+    public static void main(String[] args) {
+
+        char ch1, ch2;
+
+        ch1 = 'X';
+
+        System.out.println("ch1 is " + ch1);
+
+        ch1++;
+
+        System.out.println("ch1 is now " + ch1);
+
+        boolean b = false;
+        System.out.println(b);
+
+
+    }
+}
---
+++ src/DynamicInitialization.java
@@ -0,0 +1,9 @@
+public class DynamicInitialization {
+    public static void main(String[] args) {
+        double a = 3.0, b = 3.3;
+
+        double c = Math.sqrt(a + b);
+
+        System.out.println("this is c " + c);
+    }
+}
